379d0cad617603516b100042af3bdb5b08b759de
imsadman90 <sadmansam473@gmail.com>
2025-11-15 18:37:19 +0600

    created a framer motion

---
+++ src/layouts/MotionLayout.jsx
@@ -0,0 +1,19 @@
+// src/layouts/MotionLayout.jsx
+import { motion } from "framer-motion";
+
+
+const MotionLayout = ({ children }) => {
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 50 }}   
+      animate={{ opacity: 1, y: 0 }}   
+      exit={{ opacity: 0, y: 50 }}
+      transition={{ type: "spring", stiffness: 200, damping: 40, duration: 0.6 }}
+      className="w-full"
+    >
+      {children}
+    </motion.div>
+  );
+};
+
+export default MotionLayout;
