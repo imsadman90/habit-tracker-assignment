1219560c624a9317b02c4539e5017f7028833a11
imsadman90 <sadmansam473@gmail.com>
2025-11-15 18:38:57 +0600

    added a featured habit section with latest habits

---
+++ src/pages/Home/FeaturedHabit.jsx
@@ -0,0 +1,45 @@
+import { useLoaderData } from "react-router";
+import { HabitCard } from "../../components/HabitCard";
+import Banner from "../../components/Banner";
+import MotionLayout from "../../layouts/MotionLayout";
+import { useEffect, useState } from "react";
+
+const FeaturedHabit = () => {
+  const data = useLoaderData();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (data) {
+      setLoading(false);
+    }
+  }, [data]);
+
+  const latestHabits = data
+    .slice()
+    .sort((a, b) => new Date(b.created_at) - new Date(a.created_at))
+    .slice(0, 6);
+
+  if (loading)
+    return (
+      <div className="flex justify-center items-center">
+        <span className="loading loading-spinner loading-lg"></span>
+      </div>
+    );
+
+  return (
+    <MotionLayout>
+      <div className="">
+        <Banner />
+        <div className="text-center text-3xl font-bold mt-5 mb-10 text-black/80">Latest Habits</div>
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 px-10 lg:grid-cols-3 gap-6 mt-10">
+          {latestHabits.map((habit) => (
+            <HabitCard key={habit._id} habit={habit} />
+          ))}
+        </div>
+      </div>
+    </MotionLayout>
+  );
+};
+
+export default FeaturedHabit;
